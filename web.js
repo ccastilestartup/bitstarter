1bf984ddf3761b48dc803d3d4292f9f6c87f0206
Christine Castile <ccastile.startup@gmail.com>
2013-07-10 22:43:18 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,10 +1,16 @@
 var express = require('express');
+var fs = require('fs');
 
+var buf = new Buffer(30);
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+fs.readFileSync('index.html','utf8', function (err, data) {
+    if (err) throw err;
+    console.log(data.toString());
 });
+
+app.get('/', function(request, response) {
+  response.send(fs.readFileSync);
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
